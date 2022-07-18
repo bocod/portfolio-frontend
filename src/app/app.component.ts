c54217e8c08c320e8081c2c24253543e8e1d913c
Agustin Boccio <aboccio@hotmail.com>
2022-07-18 22:52:52 +0200

    Home component - integrated view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  templateUrl: './components/home/home.component.html',
+  styleUrls: ['./components/nav-bar/home.component.css']
 })
 export class AppComponent {
   title = 'portfolio-frontend';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileHeaderComponent } from './components/profile-header/profile-header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileHeaderComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
