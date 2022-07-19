da18ec87274c685d58016c163cd2e4081d641d54
Agustin Boccio <aboccio@hotmail.com>
2022-07-19 15:27:36 +0200

    Created path for links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'profile', component:ProfileComponent}
+  // {path: '**', component: Pagina404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
