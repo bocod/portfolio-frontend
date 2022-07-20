e9fe09e1a30f64528036c15ac270c085656fab27
Agustin Boccio <aboccio@hotmail.com>
2022-07-20 11:35:31 +0200

    navbar buttons with ngIf, added Logout button

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
 export class NavBarComponent implements OnInit {
 
   constructor() { }
