a8dc24cfdcd7cce697cfeca0973619b4ec74a3bb
Agustin Boccio <aboccio@hotmail.com>
2022-07-29 15:19:16 +0200

    New routes and redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
-  {path: '', component:HomeComponent},
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
   {path: 'login', component:LoginComponent},
   {path: 'profile', component:ProfileComponent},
   {path: '**', redirectTo: ''}
