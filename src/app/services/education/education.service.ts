d5a27120e7106c76c6df4eafa4f35de467a6431d
Agustin Boccio <aboccio@hotmail.com>
2022-08-02 16:13:16 +0200

    Get data functions for all components

--- src/app/services/education/education.service.ts
+++ src/app/services/education/education.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducationService {
+  url: string = "http://localhost:8080/education";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData():Observable<any>{
+    return this.http.get<any>(`${this.url}/view`)
+  }
 }
