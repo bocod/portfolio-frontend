7813cddcec980f392968dd84c3b8ad5277d89c8c
Agustin Boccio <aboccio@hotmail.com>
2022-08-06 20:24:06 +0200

    Update routes  and components in use

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { EditHeaderComponent } from './components/editables/edit-header/edit-header.component';
-import { EditComponent } from './components/editables/edit/edit.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -15,7 +13,6 @@ const routes: Routes = [
   {path: 'profile', component:ProfileComponent, 
   // canActivate:[GuardGuard]
   },
-  {path: 'edit', component:EditComponent},
   {path: '**', redirectTo: ''}
   // {path: '**', component: Pagina404Component}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,6 @@ import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InterceptorService } from './services/interceptor.service';
-import { EditComponent } from './components/editables/edit/edit.component';
-import { EditHeaderComponent } from './components/editables/edit-header/edit-header.component';
-import { EditAboutComponent } from './components/editables/edit-about/edit-about.component';
-import { EditExperienceComponent } from './components/editables/edit-experience/edit-experience.component';
-import { EditSkillsComponent } from './components/editables/edit-skills/edit-skills.component';
-import { EditProjectsComponent } from './components/editables/edit-projects/edit-projects.component';
 import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
@@ -35,12 +29,6 @@ import { EducationComponent } from './components/education/education.component';
     ProjectsComponent,
     LoginComponent,
     ProfileComponent,
-    EditComponent,
-    EditHeaderComponent,
-    EditAboutComponent,
-    EditExperienceComponent,
-    EditSkillsComponent,
-    EditProjectsComponent,
     EducationComponent
   ],
   imports: [
