f158cc29b99ec385dc3e46e146f67b46826d6dfa
Agustin Boccio <aboccio@hotmail.com>
2022-08-06 10:02:51 +0200

    Experience CRUD - WIP

--- src/app/services/experience/experience.service.ts
+++ src/app/services/experience/experience.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Experience } from 'src/app/classes/experience';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,22 @@ export class ExperienceService {
 
   getData():Observable<any>{
     return this.http.get<any>(`${this.url}/view`)
+  }
+
+
+  findExperience(id: number):Observable<Object>{
+    return this.http.get(`${this.url}/find/${id}`)
+  }
+
+  createExperience(newExperience: Experience): Observable<Object>{
+    return this.http.post(`${this.url}/new`, newExperience)
+  }
+
+  putExperience(exp: Experience): Observable<Object>{
+    return this.http.put(`${this.url}/put`, exp)
+  }
+
+  deleteExperience(id: number): Observable<Object>{
+    return this.http.delete(`${this.url}/delete/${id}`)
   }
 }
