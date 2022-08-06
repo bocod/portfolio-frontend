dc68a8befb20952ccce95c58c5300dd4ceac4944
Agustin Boccio <aboccio@hotmail.com>
2022-08-06 20:22:58 +0200

    CRUD for Profile

---
+++ src/app/classes/profile.ts
@@ -0,0 +1,9 @@
+export class Profile {
+    id : number | undefined;
+    firstname : string | undefined;
+    surname : string | undefined;
+    subtitle : string | undefined;
+    location : string | undefined;
+    contact : string | undefined;
+    avatar : string | undefined;
+}
