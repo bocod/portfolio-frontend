f64303f9d6e6932d2e1f57aa5f53f4d899617035
Agustin Boccio <aboccio@hotmail.com>
2022-08-17 00:03:59 +0200

    Logout button with session destroy function

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -9,9 +10,20 @@ import { AuthService } from 'src/app/core/services/auth.service';
 
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  userLog = sessionStorage.getItem("portfolioCurrentUser");
+  userParsed = this.userLog !== null ? JSON.parse(this.userLog) : undefined;
+  userLogged = this.userParsed !== undefined ? this.userParsed.authorities[0].authority == "ROLE_ADMIN" : false;
+
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+    
+  }
+
+  logout(event: Event){
+    event.preventDefault();
+    sessionStorage.removeItem("portfolioCurrentUser");
+    this.route.navigate(["/profile"]);
   }
 
 }
